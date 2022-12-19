913339caa1ad57c7796a0e4e4fd9290c589058db
Ichsan Al Munawwar Patintingan <ichsan.al.ft21@mail.umy.ac.id>
2022-12-19 16:34:07 +0700

    add public void delete

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -9,10 +9,41 @@ namespace Exercise5
     class Node
     {
         public Node ichsan;
+        private Node next;
         public Node munawwar;
-        public Node()
+        public Node(int ell)
         {
             this.ichsan = this.munawwar = null;
+        }
+        public void insert(int ell)
+        {
+            Node baru = new Node(ell);
+            if (this.munawwar == null)
+            {
+                this.ichsan = this.munawwar = baru;
+            }
+            else
+            {
+                this.munawwar.next = baru;
+                this.munawwar = baru; 
+            }
+            Console.WriteLine("Inserted", ell);
+        }
+        public void delete()
+        {
+            if(this.ichsan == null)
+            {
+                Console.WriteLine("Queue Empty");
+                return;
+            }
+            Node temp = this.ichsan;
+            this.ichsan = this.ichsan.next;
+
+            if(this.ichsan == null)
+            {
+                this.munawwar = null;
+            }
+            Console.WriteLine("Item Deleted");
         }
         static void Main(string[] args)
         {
